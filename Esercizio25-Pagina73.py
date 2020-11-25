b9ae7e1c32c3b90ee1367b8f064516a63b6162d8
Francesco-Righetti <74307547+Francesco-Righetti@users.noreply.github.com>
2020-11-25 15:25:07 +0100

    Ho aggiunto i commenti al file

--- Esercizio25-Pagina73.py
+++ Esercizio25-Pagina73.py
@@ -1,12 +1,12 @@
-candidato1 = input("Chi è il primo candidato? ")
+candidato1 = input("Chi è il primo candidato? ") #Creo le variabili e le aggiungo alla lista.
 candidato2 = input("Chi è il secondo candidato? ")
 candidati = [candidato1, candidato2]
 
-voti1 = int(input("Quanti sono i voti del primo candidato? "))
+voti1 = int(input("Quanti sono i voti del primo candidato? ")) #Creo le variabili e le aggiungo alla lista.
 voti2 = int(input("Quanti sono i voti del secondo candidato? "))
 voti = [voti1, voti2]
 
-candidati.sort()
+candidati.sort() #Ordino le liste in ordine alfabetico e ordine decrescente.
 voti.reverse()
 
 print (candidati)
