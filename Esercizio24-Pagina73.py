855581da7876258fda70ed0768036e7612846e1f
Francesco-Righetti <74307547+Francesco-Righetti@users.noreply.github.com>
2020-11-25 15:23:06 +0100

    Update Esercizio24-Pagina73.py

--- Esercizio24-Pagina73.py
+++ Esercizio24-Pagina73.py
@@ -6,15 +6,15 @@ votiBiden = int(input("Voti candidatura Biden "))
 
 votiTot = votiBiden + votiTrump
 
-percentualeTrump = votiTrump * 100 / votiTot
+percentualeTrump = votiTrump * 100 / votiTot #Calcolo percentuale dei voti di Trump.
 print("La percentuale di voti del candidato Trump = ")
 print(percentualeTrump)
 
-percentualeBiden = votiBiden * 100 / votiTot
+percentualeBiden = votiBiden * 100 / votiTot #Calcolo percentuale dei voti di Biden.
 print("La percentuale di voti del candidato Biden = ")
 print(percentualeBiden)
 
-if percentualeBiden > percentualeTrump:
+if percentualeBiden > percentualeTrump: #Decreto chi vince le elezioni.
   print("Il candidato Biden vince le elezioni")
 
 else:
