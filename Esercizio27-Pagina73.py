e355646fa0d94e7a1eeb11fbf6c1812ba2f3a95f
Francesco-Righetti <74307547+Francesco-Righetti@users.noreply.github.com>
2020-11-25 15:33:34 +0100

    Aggiungo i commenti all'esercizio 27

--- Esercizio27-Pagina73.py
+++ Esercizio27-Pagina73.py
@@ -1,17 +1,15 @@
-veicoli = []
-countergiorni = 0
+veicoli = [] #Creo la variabile.
+countergiorni = 0 #Creo un counter per tenere conto del periodo di tempo considerato per il calcolo dei veicoli transitati.
 print("Programma per calcolare il numero di veicoli transitati in un casello autostradale in un numero definito di giorni.")
 print("Inserire 0 quando si è completato il conteggio dei veicoli.")
 
-while True:
-
+while True: #Creo il ciclo while per l'acquisizione dei veicoli transitati in un arco definito di tempo.
     veicolo = int(input("Quanti veicoli hanno transitato in entrata di questo casello stradale? "))
     if veicolo == 0:
         break
-
     else:
         countergiorni += 1
         veicoli.append(veicolo)
 
-totveicoli = (sum(veicoli))
+totveicoli = (sum(veicoli)) #Calcolo la somma dei veicoli totali transitati in un arco definito di tempo.
 print("Il totale dei veicoli che hanno transitato in autostrada in", countergiorni, "giorni =", totveicoli)
