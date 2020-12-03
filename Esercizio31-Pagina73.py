458aaab0fbe133d54b77dfb90ccc51cc3b373fd8
Francesco-Righetti <74307547+Francesco-Righetti@users.noreply.github.com>
2020-12-03 08:12:32 +0100

    Modifica esercizio 31:
    Ho aggiunto il testo dell'esercizio al file sotto forma di commento.

--- Esercizio31-Pagina73.py
+++ Esercizio31-Pagina73.py
@@ -1,3 +1,8 @@
+#Fornisci la rappresentazione in binario di un numero decimale. Dpo aver acquisito il valore del Numero da trasformare, se esegue la divisione del numero per 2, e si calcola quoziente e resto.
+#Il resto è la orima cifra della rappresentazione binaria. SI ripete il procedimento assegnando il quoziente ottenuto a Numero e ricalcolando la divisione per 2.
+#La ripetizione prosegue mentre il quoziente ottenuto si mantiene diverso da 0. La rappresentazione binaria del numero decimale è costituita dalle cifre binarie ottenute come resti, lette in ordine inverso.
+#Confronta poi il risultato con il valore ottenuto dalla funzione predefinita del linguaggio per convertire un numero decimale in binario.
+
 print("Programma per convertire un numero decimale in binario. Ricordarsi che i numeri binari comprendono solo cifre 1 e 0.")
 print()
 
