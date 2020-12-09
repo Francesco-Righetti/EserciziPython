76b0965b7258ac00c83678ea1f82513bad210f30
Francesco-Righetti <fre.righetti@gmail.com>
2020-12-09 20:43:03 +0100

    Correggo l'eserczio 31

--- Esercizio31-Pagina73.py
+++ Esercizio31-Pagina73.py
@@ -1,19 +1,22 @@
-#Fornisci la rappresentazione in binario di un numero decimale. Dpo aver acquisito il valore del Numero da trasformare, se esegue la divisione del numero per 2, e si calcola quoziente e resto.
-#Il resto è la orima cifra della rappresentazione binaria. SI ripete il procedimento assegnando il quoziente ottenuto a Numero e ricalcolando la divisione per 2.
+#Fornisci la rappresentazione in binario di un numero decimale. Dopo aver acquisito il valore del Numero da trasformare, se esegue la divisione del numero per 2, e si calcola quoziente e resto.
+#Il resto è la prima cifra della rappresentazione binaria. SI ripete il procedimento assegnando il quoziente ottenuto a Numero e ricalcolando la divisione per 2.
 #La ripetizione prosegue mentre il quoziente ottenuto si mantiene diverso da 0. La rappresentazione binaria del numero decimale è costituita dalle cifre binarie ottenute come resti, lette in ordine inverso.
 #Confronta poi il risultato con il valore ottenuto dalla funzione predefinita del linguaggio per convertire un numero decimale in binario.
 
-print("Programma per convertire un numero decimale in binario. Ricordarsi che i numeri binari comprendono solo cifre 1 e 0.")
+print("Programma per convertire un numero decimale in binario. Ricorda che i numeri binari comprendono solo cifre 1 e 0.")
 print()
 
 numero_decimale = int(input("Inserire il numero da trasformare da decimale a binario. ")) #Creo l'input per il numero decimale.
-resti_binario = [] #Creo la lista.
+binario = [] #Creo la lista.
+resto = 0
+quoziente = 0
+x = numero_decimale 
 
-while numero_decimale != 0:
-    quoziente = numero_decimale // 2
-    resto = numero_decimale % 2
-    resti_binario.append(quoziente)
+while x != 0:
+    resto = x % 2
+    x = x // 2
+    binario.append(resto)
 
-resti_binario.reverse() #Inverto la lista del resto del numero decimale, riassumo e confronto.
+binario.reverse() #Inverto la lista del resto del numero decimale, riassumo e confronto.
 print("Il numero tradotto con il comando python risulta", bin(numero_decimale))
-print("Il numero", numero_decimale, "tradotto con il mio programma risulta",resti_binario)
+print("Il numero", numero_decimale, "tradotto con il mio programma risulta", binario)
