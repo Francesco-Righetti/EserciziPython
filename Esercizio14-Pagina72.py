f88cfee96f16145e33aff4b2a20c322c9b34f38b
Francesco-Righetti <74307547+Francesco-Righetti@users.noreply.github.com>
2020-12-25 20:56:35 +0100

    Ho aggiunto i commenti al file.

--- Esercizio14-Pagina72.py
+++ Esercizio14-Pagina72.py
@@ -4,10 +4,10 @@
 numero_a = 0
 numero_b = 0
 
-numero_a = int(input("Inserire il primo valore: "))
+numero_a = int(input("Inserire il primo valore: ")) #Creo gli input per entrambi i valori.
 numero_b = int(input("Inserire il secondo valore: "))
 
-if (numero_a * numero_b) > 10:
+if (numero_a * numero_b) > 10: #Faccio i prodotti in ogni statement del ciclo if/elif e poi stampo la differenza/somma dei due numeri.
     print("Il prodotto di questi due numeri è maggiore di 10, la differenza tra questi due numeri è quindi:", numero_a - numero_b)
 elif (numero_a * numero_b) <= 10:
     print("Il prodotto di questi due numeri è minore o uguale di 10, la somma tra questi due numeri è quindi:", numero_a + numero_b)
