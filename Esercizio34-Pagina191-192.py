7d1ba65f96a6f7176ca04313db7dcbed6828dd34
Francesco-Righetti <fre.righetti@gmail.com>
2021-01-31 17:03:42 +0100

    Aggiorno l'esercizio 34

--- Esercizio34-Pagina191-192.py
+++ Esercizio34-Pagina191-192.py
@@ -7,13 +7,21 @@ Scrivi un programma che comprenda due funzionalità:
 La funzione che produce l'elenco deve anche aggiornare l'elenco deve anche aggiornare l'elenco dei partecipanti ai quali è già stata inviata la lettera.
 '''
 lista_dati = []
+lista_lettere_si = []
 
 while True: #Soddisfo il primo punto della consegna, creo un ciclo while True e aggiungo il nome dei partecipanti alla lista apposita
-  dati_partecipanti = input("Inserire il nome dei partecipanti:")
+  dati_partecipanti = input("(Digitare 'STOP' per terminare) Inserire il nome dei partecipanti: ").upper()
+  lettera = input("(Digitare 'STOP' per terminare) Bisogna inviare la lettera al partecipante? ")
+
   if dati_partecipanti == "STOP":
     break
-
   else:
     lista_dati.append(dati_partecipanti)
-
-
+  
+  if lettera == "sì":
+    lista_lettere_si.append(lista_dati.pop(lista_dati.index(dati_partecipanti))) #Ho aggiunto a "lista_lettere_si" l'elemento estratto dalla lista_dati che ha lo stesso indice di dati_partecipanti in lista_dati
+  elif lettera == "no":
+    pass
+print("Le prenotazioni per il convegno sono:", lista_dati)
+print("La lista dei partecipanti è:", lista_dati)
+print("La lista dei partecipanti a cui mandare la lettera è:", lista_lettere_si)
