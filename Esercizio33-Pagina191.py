b4b8f32f854dcda047420fb5a57c0f55b6edbda2
Francesco-Righetti <fre.righetti@gmail.com>
2021-01-31 16:10:03 +0100

    Aggiorno esercizio 33 e 34

--- Esercizio33-Pagina191.py
+++ Esercizio33-Pagina191.py
@@ -1,18 +1,19 @@
 '''
-In un laboratorio di analisi i pazienti sono sottoposti ad un esame specialistico secondo l'ordine di arrivo (suggerimento: utilizza una struttura di pila per memorizzare i nomi dei paziente): scrivi il programma che consenta di registrare i nomi dei pazienti che man mano arrivano. Visualizza poi il nome del paziente che deve essere sottoposto all'esame perché è il della coda in attesa.
+In un laboratorio di analisi i pazienti sono sottoposti ad un esame specialistico secondo l'ordine di arrivo 
+(suggerimento: utilizza una struttura di pila per memorizzare i nomi dei paziente): 
+scrivi il programma che consenta di registrare i nomi dei pazienti che man mano arrivano. 
+Visualizza poi il nome del paziente che deve essere sottoposto all'esame perché è il della coda in attesa.
 '''
 coda_pazienti = []
 
-while True:
+while True: #Creo un ciclo while True per aggiungere l'input dei nomi dei pazienti, e li aggiunge alla apposita lista.
     print("Per smettere di aggiornare la coda digitare 'STOP'.")
-    input_pazienti = input(
-        "Inserire il nome del paziente che si vuole aggiungere alla coda: ").upper()
+    input_pazienti = input("Inserire il nome del paziente che si vuole aggiungere alla coda: ").upper()
     if input_pazienti == "STOP":
         break
 
     else:
         coda_pazienti.append(input_pazienti)
 
-print()
-for paziente in coda_pazienti:
+for paziente in coda_pazienti: #Stampa il turno del prossimo paziente da sottoporre all'esame.
     print("Ora è il turno di", paziente)
--- Esercizio34-Pagina191-192.py
+++ Esercizio34-Pagina191-192.py
@@ -6,4 +6,14 @@ Scrivi un programma che comprenda due funzionalità:
       eliminando quelli ai quali la lettera è già stata inviata e che sono registrati in un apposito elenco.
 La funzione che produce l'elenco deve anche aggiornare l'elenco deve anche aggiornare l'elenco dei partecipanti ai quali è già stata inviata la lettera.
 '''
+lista_dati = []
+
+while True: #Soddisfo il primo punto della consegna, creo un ciclo while True e aggiungo il nome dei partecipanti alla lista apposita
+  dati_partecipanti = input("Inserire il nome dei partecipanti:")
+  if dati_partecipanti == "STOP":
+    break
+
+  else:
+    lista_dati.append(dati_partecipanti)
+
 
