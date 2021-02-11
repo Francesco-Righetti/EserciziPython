7e51dec96708c6130394b1374d665a689e47e2ee
Francesco-Righetti <fre.righetti@gmail.com>
2021-02-11 08:53:32 +0100

    Aggiungo esercizio 25

--- Esercizio25-Pagina190.py
+++ Esercizio25-Pagina190.py
@@ -1,13 +1,14 @@
 '''
 I voti assegnati a 30 studenti di una classe in una prova di italiano sono memorizzati in un dizionario che ha per chiave la matricola, mentre il valore associato è il voto.
-Elenca i risultati in ordine crescente di voto e successivamente visualizza quali voti diversi tra loro sono stati assegnati, riducendo a uno i voti uguali. '''
+Elenca i risultati in ordine crescente di voto e successivamente visualizza quali voti diversi tra loro sono stati assegnati, riducendo a uno i voti uguali. 
+'''
 
 voti_studenti = {}
 counter_studente = 0
 
 while True:
 
-  print("Studente numero:",counter_studente,)
+  print("Studente numero:", counter_studente)
   counter_studente += 1
   voto = float(input("Qual è il voto dello studente? "))
   voti_studenti[counter_studente] = voto
@@ -20,12 +21,5 @@ while True:
     pass
   
   if counter_studente == 4:
-    print("Il voto più alto in classe è stato:")
-    print(max(voti_studenti.values()))
+    print("I voti in ordine crescente sono:", sorted(voti_studenti.values()))
     break
-
-  else:
-    pass
-
-print(voti_studenti.values())
-
